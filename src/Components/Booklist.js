b9c54aab7f085b29cc8071a4b1dc9dfcc75e8260
Melissa Chiang <mchiang62@gmail.com>
2021-02-04 15:28:12 -0500

    working more with props

--- src/Components/Booklist.js
+++ src/Components/Booklist.js
@@ -1,44 +1,55 @@
 import React from "react";
 import './Booklist.css'
 
+
+const title = "The Subtle Art of Not Giving a F**k"
+//this is not hard coding anymore
+const author = "Mark Manson"
+const imgBook = 'https://books.google.com/books/publisher/content/images/frontcover/yng_CwAAQBAJ?fife=w200-h300'
+
+const firstBook = {
+    title: "The Subtle Art of Not Giving a F**k",
+    author: "Mark Manson",
+    imgBook: 'https://books.google.com/books/publisher/content/images/frontcover/yng_CwAAQBAJ?fife=w200-h300'
+}
+
+
+
 const Booklist = () => {
     return (
         <section className='bList'>
-            <Book />
-            <Book />
-            <Book />
-            <Book />
-            <Book />
-            <Book />
+            <Book imgBook={imgBook}/>
+            <Book title={firstBook.title}/>
+            <Book author={author}/>
         
         </section>
     )
 }
 
-const Book = () => {
-    return (
-    <article className='book'>
-        BOOK! :)
 
-        <Title />
-        <Author />
+
+const Book = (props) => {
+    
+
+    return (
+
+    <article className='book'>
+        <img src={props.imgBook} />
+        <h4 style={{fontWeight:'bold'}}>{props.title}</h4>
+        <p style={{color:'red', fontSize:'12pt'}}>{props.author}</p>
+
 
 
     </article>
      
     )}
 
-const Title = () => {
-    return <h4>love you to the moon and back</h4>
 
-}
-
-const Author = () => {
-    return <p style={{color:'red', fontSize:'10pt'}}>Amelia</p>
-    // this is called in line css. this will override css file. inner {}brackets are objects. outer ones are JS
-
-}
 
 
 
 export default Booklist;
+
+
+{/* <p style={{color:'red', fontSize:'12pt'}}>Mark Manson</p> */}
+{/* this is called in line css. this will override css file. inner {}brackets are objects. outer ones are JS */}
