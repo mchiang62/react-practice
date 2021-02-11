a6e492e5c465722670382c9e8e9a5fbfa4e6b47c
Melissa Chiang <mchiang62@gmail.com>
2021-02-11 10:07:57 -0500

    rendered booklist

--- src/Components/Booklist.js
+++ src/Components/Booklist.js
@@ -2,26 +2,50 @@ import React from "react";
 import './Booklist.css'
 
 
-const title = "The Subtle Art of Not Giving a F**k"
-//this is not hard coding anymore
-const author = "Mark Manson"
-const imgBook = 'https://books.google.com/books/publisher/content/images/frontcover/yng_CwAAQBAJ?fife=w200-h300'
-
-const firstBook = {
+const books = [{
+    id: 1,
     title: "The Subtle Art of Not Giving a F**k",
     author: "Mark Manson",
     imgBook: 'https://books.google.com/books/publisher/content/images/frontcover/yng_CwAAQBAJ?fife=w200-h300'
+},
+{
+    id: 2,
+    title: "Where the Crawdads Sing",
+    author: "Delia Owens",
+    imgBook: 'https://m.media-amazon.com/images/I/51j5p18mJNL._SY346_.jpg'
+},
+
+{
+    id: 3,
+    title: "The Book of Two Ways: A Novel",
+    author: "Jodi Picoult",
+    imgBook: 'https://m.media-amazon.com/images/I/51EF8oBnYcL._SY346_.jpg'
 }
+
+
+]
 
 
 
 const Booklist = () => {
     return (
         <section className='bList'>
-            <Book imgBook={imgBook}/>
-            <Book title={firstBook.title}/>
-            <Book author={author}/>
-        
+            {/* <Book imgBook={firstBook.imgBook} title={firstBook.title} author={firstBook.author}/> */}
+
+            {books.map((book)=> {
+                const {id, title, author, imgBook} = book;
+                return (
+                <Book 
+                    key={id}
+                    img={imgBook}
+                    title={title}
+                    author={author}
+                    />
+
+                )
+
+            })}
+           
         </section>
     )
 }
@@ -29,15 +53,26 @@ const Booklist = () => {
 
 
 const Book = (props) => {
-    
+    //object destructuring - not just for react - it's JS ({img, title, author})
+    // const {img, title, author} = props.book
 
+    const clickHandler = () => {
+        alert('hiiiiii Mel')
+    }
+    
     return (
 
     <article className='book'>
-        <img src={props.imgBook} />
+        <img src={props.img} />
         <h4 style={{fontWeight:'bold'}}>{props.title}</h4>
+        {props.children}
         <p style={{color:'red', fontSize:'12pt'}}>{props.author}</p>
-
+        <button type="button" onClick={clickHandler}>HI</button>
+        {/* <button type="button" onClick={() => clickHandler(info)}>HI</button> 
+        if you need to pass the arguments then you gotta create another function in line
+        */}
+        
+        
 
 
     </article>
